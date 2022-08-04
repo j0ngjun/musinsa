0ea43842a3146eb38e79d969efce9a23753705fa
KwonJongjun <j0ngjun@kyobobook.com>
2022-08-04 09:48:57 +0900

    Initial commit

---
+++ src/test/java/com/musinsa/MusinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
